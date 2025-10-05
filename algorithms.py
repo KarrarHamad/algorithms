c3b2822618fafdd232a166a07e1f31de717382b7
Ali <alihamad313313@gmail.com>
2025-10-05 21:50:34 +0300

    Taking Notes

---
+++ algorithms.py
@@ -0,0 +1,8 @@
+# Write a factorial function
+# Write a fin maximum in a list function
+# Write linear search function
+# Write a fibonacci sequence function
+# Write a simple menu to choose between functionalities
+# Ask the user for input and call the correct function
+# block logget out users
+# mark big o
